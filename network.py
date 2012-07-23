37b355d84a7381cf381b27f98c464d4c620030c4
ejrh <ejrh00@gmail.com>
2012-07-23 10:46:43 +0000

    Use different heats for different seeds.

--- network.py
+++ network.py
@@ -8,16 +8,44 @@ import random
 import pygame
 
 
+def heat_flow(h1, h2):
+    a = max(0, h1[0] - h2[0]) / 2.0
+    b = max(0, h1[1] - h2[1]) / 2.0
+    c = max(0, h1[2] - h2[2]) / 2.0
+    return a,b,c
+
+
+def heat_add(h1, h2):
+    a = h1[0] + h2[0]
+    b = h1[1] + h2[1]
+    c = h1[2] + h2[2]
+    return a,b,c
+
+
+def heat_collar(h):
+    a = max(0.0, min(h[0], 1.0))
+    b = max(0.0, min(h[1], 1.0))
+    c = max(0.0, min(h[2], 1.0))
+    return a,b,c
+
+
 class Tile(object):
-    def __init__(self, x, y, ports=[False]*4, source=False):
+    def __init__(self, x, y, ports=[False]*4, source=None):
         self.x = x
         self.y = y
         self.ports = ports[:]
         self.source = source
         self.true_ports = ports[:]
         self.locked = False
-        self.heat = 0.0
-        self.prev_heat = 0.0
+        if source == 0:
+            self.heat = (1.0, 0.0, 0.0)
+        elif source == 1:
+            self.heat = (0.0, 1.0, 0.0)
+        elif source == 2:
+            self.heat = (0.0, 0.0, 1.0)
+        else:
+            self.heat = (0.0, 0.0, 0.0)
+        self.prev_heat = (0.0, 0.0, 0.0)
         self.possibles = [True]*4
         self.necessaries = [False]*4
         self.dead_end = False
@@ -25,10 +53,10 @@ class Tile(object):
         self.mark = False
         self.ai_rotations = 0
 
-
     def rotate(self, steps=1):
         self.ports = self.ports[steps:] + self.ports[:steps]
-        self.heat = 0.0
+        if self.source is None:
+            self.heat = (0.0, 0.0, 0.0)
 
     def clone(self):
         t = Tile(self.x, self.y)
@@ -54,7 +82,7 @@ class Map(object):
         self.width = self.DEFAULT_WIDTH
         self.height = self.DEFAULT_HEIGHT
         self.torus = False
-        self.num_seeds = 1
+        self.num_seeds = num_seeds
         self.clear(width, height, torus, num_seeds)
 
 
@@ -92,7 +120,7 @@ class Map(object):
         for k in range(self.num_seeds):
             i = random.randint(0, self.height - 1)
             j = random.randint(0, self.height - 1)
-            t = Tile(j, i, source=True)
+            t = Tile(j, i, source=k%3)
             self.tiles[i][j] = t
             seeds.append(t)
 
@@ -118,7 +146,7 @@ class Map(object):
             for t in row:
                 t.true_ports = t.ports[:]
                 t.rotate(random.randint(0,3))
-                if sum(t.ports) == 1 and not t.source:
+                if sum(t.ports) == 1 and t.source is None:
                     self.sinks += 1
                 if sum(t.ports) == 0:
                     self.locked = True
@@ -162,22 +190,18 @@ class Map(object):
     def update(self):
         adj = []
         for i in range(self.height):
-            adj.append([-0.01]*self.width)
+            adj.append([(-0.01, -0.01, -0.01)]*self.width)
 
         for i in range(self.height):
             for j in range(self.width):
                 if self.tiles[i][j] in [None,False]:
                     continue
-                if self.tiles[i][j].source:
-                    adj[i][j] = 1 - self.tiles[i][j].heat
-                else:
+                if self.tiles[i][j].source is None:
                     ns = self.get_neighbours(self.tiles[i][j])
                     for k in range(4):
                         if ns[k] not in [False,None] and self.tiles[i][j].ports[k] and ns[k].ports[(k + 2) % 4]:
-                            d = (ns[k].heat - self.tiles[i][j].heat)/2.0
-                            if d < 0:
-                                d = 0
-                            adj[i][j] += d
+                            d = heat_flow(ns[k].heat, self.tiles[i][j].heat)
+                            adj[i][j] = heat_add(adj[i][j], d)
 
         changed = False
         self.lights = 0
@@ -187,19 +211,13 @@ class Map(object):
                 tile = self.tiles[i][j]
                 if tile in [None,False]:
                     continue
-                tile.prev_heat = tile.heat
-                tile.heat += adj[i][j]
-                changed2 = adj[i][j] != 0.0
-                if tile.heat < 0:
-                    tile.heat = 0
-                    changed2 = False
-                if tile.heat > 1.0:
-                    tile.heat = 1.0
-                    changed2 = False
-                changed |= changed2
+                if tile.source is None:
+                    tile.prev_heat = tile.heat
+                    tile.heat = heat_collar(heat_add(tile.heat, adj[i][j]))
+                    changed |= (tile.heat != tile.prev_heat)
 
-                if sum(tile.ports) == 1 and not tile.source:
-                    if tile.heat > 0.0 and tile.heat >= tile.prev_heat:
+                if sum(tile.ports) == 1 and tile.source is None:
+                    if tile.heat != (0.0, 0.0, 0.0) and sum(tile.heat) >= sum(tile.prev_heat):
                         self.lights += 1
 
                 if tile.locked:
@@ -274,9 +292,9 @@ class Window(object):
             pygame.draw.rect(self.display, (50, 100, 150), pygame.Rect(col*50+1, row*50+1, 48, 48))
         else:
             pygame.draw.rect(self.display, (150, 100, 50), pygame.Rect(col*50+1, row*50+1, 48, 48))
-        wire_col = [tile.heat*200, 50, 200-tile.heat*200]
-        if tile.heat < tile.prev_heat:
-            wire_col[1] = 200
+        wire_col = [tile.heat[0]*255, tile.heat[1]*255, tile.heat[2]*255]
+        if sum(tile.heat) < sum(tile.prev_heat):
+            wire_col = (0, 0, 0)
         if tile.ports[0]:
             pygame.draw.rect(self.display, wire_col, pygame.Rect(col*50+21, row*50+1, 8, 24))
         if tile.ports[1]:
@@ -285,13 +303,13 @@ class Window(object):
             pygame.draw.rect(self.display, wire_col, pygame.Rect(col*50+21, row*50+25, 8, 24))
         if tile.ports[3]:
             pygame.draw.rect(self.display, wire_col, pygame.Rect(col*50+25, row*50+21, 24, 8))
-        if tile.source:
+        if tile.source is not None:
             pygame.draw.circle(self.display, (200, 150, 50), (col*50+25, row*50+25), 12)
         if any(tile.ports):
             pygame.draw.circle(self.display, wire_col, (col*50+25, row*50+25), 4)
-        if sum(tile.ports) == 1 and not tile.source:
+        if sum(tile.ports) == 1 and tile.source is None:
             pygame.draw.rect(self.display, (100, 200, 100), pygame.Rect(col*50+11, row*50+11, 28, 28))
-            if tile.heat > 0.0 and tile.heat >= tile.prev_heat:
+            if sum(tile.heat) > 0.0 and sum(tile.heat) >= sum(tile.prev_heat):
                 pygame.draw.rect(self.display, (250, 250, 0), pygame.Rect(col*50+21, row*50+21, 8, 8))
         #text = self.font.render("%0.2f" % tile.heat, False, (255,255,255))
         #self.display.blit(text, (col*50, row*50))
@@ -314,9 +332,9 @@ class Window(object):
         if tile.mark:
             bg_col = (255, 255, 255)
         pygame.draw.rect(self.display, bg_col, pygame.Rect(col*6+offset_x, row*6+offset_y, 6, 6))
-        wire_col = [tile.heat*200, 50, 200-tile.heat*200]
-        if tile.heat < tile.prev_heat:
-            wire_col[1] = 200
+        wire_col = [tile.heat[0]*255, tile.heat[1]*255, tile.heat[2]*255]
+        if sum(tile.heat) < sum(tile.prev_heat):
+            wire_col = (0, 0, 0)
         if tile.ports[0]:
             pygame.draw.rect(self.display, wire_col, pygame.Rect(col*6+offset_x+2, row*6+offset_y, 2,4))
         if tile.ports[1]:
@@ -325,9 +343,9 @@ class Window(object):
             pygame.draw.rect(self.display, wire_col, pygame.Rect(col*6+offset_x+2, row*6+offset_y+2, 2,4))
         if tile.ports[3]:
             pygame.draw.rect(self.display, wire_col, pygame.Rect(col*6+offset_x+2, row*6+offset_y+2, 4,2))
-        if sum(tile.ports) == 1 and not tile.source:
+        if sum(tile.ports) == 1 and tile.source is None:
             pygame.draw.rect(self.display, (100, 200, 100), pygame.Rect(col*6+offset_x+2, row*6+offset_y+2, 2,2))
-            if tile.heat > 0.0 and tile.heat >= tile.prev_heat:
+            if sum(tile.heat) > 0.0 and sum(tile.heat) >= sum(tile.prev_heat):
                 pygame.draw.rect(self.display, (250, 250, 0), pygame.Rect(col*6+offset_x+2, row*6+offset_y+2, 2,2))
 
     def draw_map(self):
@@ -431,7 +449,7 @@ class AI(object):
                 self.solve_stack = []
                 return True
 
-            if tile.source:
+            if tile.source is not None:
                 if self.set_live(tile): return True
 
             if sum(tile.ports) == 1:
@@ -590,7 +608,7 @@ def main(args=None):
     display = pygame.display.set_mode((WINDOW_WIDTH * 50, WINDOW_HEIGHT * 50))
     pygame.time.set_timer(pygame.USEREVENT+1, 250)
 
-    map = Map(21,21, True, 1)
+    map = Map(21,21, True, 3)
     window = Window(map, WINDOW_WIDTH, WINDOW_HEIGHT, display)
 
     map.generate()
